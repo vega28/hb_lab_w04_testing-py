c96997ed8c1c0f92819f66998993696cfc7d8a34
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-03-15 20:03:05 +0000

    Complete doctests for is_mel and most_and_least_common_type functions.
    Co-authored-by: Deborah Ong <deborah.oyt@gmail.com>

--- party.py
+++ party.py
@@ -8,13 +8,34 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+    
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Mel', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.edu.com')
+    True
+
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
+
+    >>> treats = [
+    ... {'type': 'dessert'},
+    ... {'type': 'dessert'},
+    ... {'type': 'appetizer'},
+    ... {'type': 'dessert'},
+    ... {'type': 'appetizer'},
+    ... {'type': 'drink'},
+    ... ]
+
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'drink')
 
     Return most and least common treat types in tuple of format (most, least).
     """
